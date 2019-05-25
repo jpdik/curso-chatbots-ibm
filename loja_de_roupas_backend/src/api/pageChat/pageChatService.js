05754626500b2fe8500abf52f6a44a694f67651a
João Paulo <jpmdik@gmail.com>
2019-05-25 18:22:32 -0300

    branch aulas

--- loja_de_roupas_backend/src/api/pageChat/pageChatService.js
+++
@@ -1,6 +0,0 @@
-const PageChat = require('./pageChat') // Obtém o esquema de produtos (modelo de dados)
-
-PageChat.methods(['get', 'post', 'put', 'delete']) //informa quais tipos de requisições serão permitidos 
-PageChat.updateOptions({new: true, runValidators: true}) //Somente retorne o objeto novo inserido e use as validações do esquema
-
-module.exports = PageChat //Exporta o serviço completo
--- loja_de_roupas_backend/src/config/database.js
+++
@@ -1,5 +0,0 @@
-// Cria uma conexão com o banco de dados e exporta.
-
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/loja_de_roupas', {useMongoClient: true})
